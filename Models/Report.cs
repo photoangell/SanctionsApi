f000552cd362f568429ec184482001f7c0545f5c
Stephen Angell <photoangell@gmail.com>
2019-11-23 00:27:48 +0000

    comments clean

--- Models/Report.cs
+++ Models/Report.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 namespace SanctionsApi.Models
 {
     public class Report{
-        //properties
         public ResultSummary resultSummary;
         public List<Dictionary<string, string>> record = new List<Dictionary<string, string>>();
 
-        //constructors
         public Report() {
             resultSummary = new ResultSummary();
             record = new List<Dictionary<string,string>>();
