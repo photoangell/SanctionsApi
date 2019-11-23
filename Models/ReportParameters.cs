bd5dd06f00f14cdeeaf3af58c0086f08b654f4df
Stephen Angell <photoangell@gmail.com>
2019-11-23 18:19:24 +0000

    initialise object

--- Models/ReportParameters.cs
+++ Models/ReportParameters.cs
@@ -2,6 +2,6 @@ using System.Collections.Generic;
 
 namespace SanctionsApi.Models {
     public class ReportParameters {
-        public List<string> HeaderFields { get; set; }
+        public List<string> HeaderFields { get; set; } = new List<string>();
     }
 }
