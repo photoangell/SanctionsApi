c18db9415a87bb321ae01b9cd98bf02eda88347e
Stephen Angell <photoangell@gmail.com>
2022-05-10 13:21:20 +0100

    add swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
@@ -12,8 +14,18 @@ builder.Services.AddCors(options =>
         builder => { builder.AllowAnyOrigin(); });
 });
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "SanctionsApi", Version = "v1" });
+});
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "SanctionsApi"); });
+}
 
 app.UseCors(CorsOriginsSetup);
 app.UseHttpsRedirection();
