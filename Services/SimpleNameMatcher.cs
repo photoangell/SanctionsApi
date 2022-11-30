036c46d6c49ab20964ba71bb17487be2aa5c2059
Stephen Angell <stephen.angell@legal-contingency.co.uk>
2022-11-30 12:08:35 +0000

    Add debug logging to simple name matcher

--- Services/SimpleNameMatcher.cs
+++ Services/SimpleNameMatcher.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using SanctionsApi.Models;
 
 namespace SanctionsApi.Services;
 
 public class SimpleNameMatcher : ISimpleNameMatcher
 {
+    private readonly ILogger<SimpleNameMatcher> _logger;
+
+    public SimpleNameMatcher(ILogger<SimpleNameMatcher> logger)
+    {
+        _logger = logger;
+    }
+
     public bool Execute(IEnumerable<FullName> fullNames, IReadOnlyList<string> row)
     {
         return fullNames.Any(fullName => IsFullNameInRow(fullName, row));
     }
 
-    private static bool IsFullNameInRow(FullName fullName, IEnumerable<string> row)
+    private bool IsFullNameInRow(FullName fullName, IEnumerable<string> row)
     {
         var countMatchedNames = row.SelectMany(r => r.Split(' '))
             .Distinct()
@@ -20,6 +28,12 @@ public class SimpleNameMatcher : ISimpleNameMatcher
                 n => n.ToLower(),
                 (r, _) => new { r })
             .Count();
+
+        if (countMatchedNames >= fullName.MaxAllowedCount)
+        {
+            _logger.LogDebug("Found {countMatchedNames} matches for fullname {fullName}", countMatchedNames, fullName);
+            _logger.LogDebug("\tRow contents: {row}", row);
+        }
 
         return countMatchedNames >= fullName.MaxAllowedCount;
     }
