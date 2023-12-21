12dffa8b1aac83f4945d655063bcd75536909b2b
Stephen Angell <stephen.angell@legal-contingency.co.uk>
2023-12-21 13:40:50 +0000

    Refactor MLNameMatcher and add non-ML execute method
    The MLNameMatcher service has been refactored to better organize the execute methods. A non-ML execute method that uses Levenshtein distance for comparing names has been added. This provides an alternative method for matching names when machine learning techniques are not suitable or desired. The threshold for matching in the ML execute method has also been lowered.

--- Services/MLNameMatcher.cs
+++ Services/MLNameMatcher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Microsoft.ML;
 using SanctionsApi.Models;
@@ -9,21 +10,21 @@ namespace SanctionsApi.Services;
 public class MLNameMatcher : INameMatcher
 {
     private readonly ILogger<MLNameMatcher> _logger;
-
+    
     public MLNameMatcher(ILogger<MLNameMatcher> logger)
     {
         _logger = logger;
     }
     
-    public bool Execute(IEnumerable<FullName> fullNames, IReadOnlyList<string> row)
+    public bool ExecuteWithML(IEnumerable<FullName> fullNames, IReadOnlyList<string> row)
     {
+        double threshold = 0.5;
         // Prepare your data
         var mlContext = new MLContext();
         var transformedData = PrepareData(mlContext, fullNames, row);
     
         // Now you can compare names in a more sophisticated way
         var data = mlContext.Data.CreateEnumerable<TransformedData>(transformedData, reuseRowObject: false);
-        double threshold = 0.9;
         
         foreach (var item in data)
         {
@@ -38,6 +39,54 @@ public class MLNameMatcher : INameMatcher
     
         //_logger.LogInformation("No match found");
         return false; // Or true depending on your comparison logic
+    }
+    
+    public bool Execute(IEnumerable<FullName> fullNames, IReadOnlyList<string> row)
+    {
+        var threshold = 3;
+        foreach(var fullName in fullNames)
+        {
+            bool allPartsMatched = true;
+
+            // Compare all name parts with the row strings
+            foreach(var namePart in fullName.NameParts)
+            {
+                var namePartLower = namePart.ToLower();
+                bool namePartMatched = false;
+
+                foreach(var s in row)
+                {
+                    var sLower = s.ToLower();
+                    // if (s.ToLower().Contains("naumov") && namePart.ToLower().Contains("naumov"))
+                    // {
+                    //     Debugger.Break();
+                    //     
+                    // }
+
+                    var distance = GetLevenshteinDistance(namePartLower, sLower);
+                    if(distance < threshold)
+                    {
+                        namePartMatched = true;
+                        break; // break loop if this name part matched
+                    }
+                }
+
+                // If a name part isn't matched, break the main loop
+                if (!namePartMatched)
+                {
+                    allPartsMatched = false;
+                    break;
+                }
+            }
+
+            // If all parts were matched, this FullName is considered matched
+            if (allPartsMatched)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
     
     public IDataView PrepareData(MLContext mlContext, IEnumerable<FullName> fullNames, IReadOnlyList<string> row)
@@ -80,6 +129,30 @@ public class MLNameMatcher : INameMatcher
         float magnitudeAMagnitudeB = (float)Math.Sqrt(magnitudeA) * (float)Math.Sqrt(magnitudeB);
         float similarity = dotProduct / magnitudeAMagnitudeB;
         return similarity;
+    }
+    
+    public static int GetLevenshteinDistance(string string1, string string2)
+    {
+        if (string1 == null) throw new ArgumentNullException("string1");
+        if (string2 == null) throw new ArgumentNullException("string2");
+        int diff;
+        var matrix = new int[string1.Length + 1, string2.Length + 1];
+        for (var i = 0; i <= string1.Length; i++)
+            matrix[i, 0] = i;
+        for (var j = 0; j <= string2.Length; j++)
+            matrix[0, j] = j;
+        for (var i = 1; i <= string1.Length; i++)
+        for (var j = 1; j <= string2.Length; j++)
+        {
+            if (string1[i - 1] == string2[j - 1])
+                diff = 0;
+            else
+                diff = 1;
+            matrix[i, j] = Math.Min(
+                Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                matrix[i - 1, j - 1] + diff);
+        }
+        return matrix[string1.Length, string2.Length];
     }
 }
 
