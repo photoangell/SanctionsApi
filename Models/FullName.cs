f886906218ea5d3e98ba3a685fd0e949129d1c55
Stephen Angell <stephen.angell@legal-contingency.co.uk>
2023-12-21 13:40:37 +0000

    Add NameParts property to FullName model
    A new property, NameParts, has been added to the FullName model. This property splits each name in the Names collection into its constituent parts, providing an easy to use collection of all name parts.

--- Models/FullName.cs
+++ Models/FullName.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SanctionsApi.Models;
 
@@ -7,6 +8,7 @@ public class FullName
 {
     public ICollection<string> Names { get; } = new List<string>();
     public int MaxAllowedCount => Names.Count > 2 ? 2 : Names.Count;
+    public IEnumerable<string> NameParts => Names.SelectMany(name => name.Split(' '));
 
     public override string ToString()
     {
