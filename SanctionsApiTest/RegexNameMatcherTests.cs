189de6916cb49ab1924d9fa42a0271e6d4ae6d21
Stephen Angell <stephen.angell@legal-contingency.co.uk>
2024-04-10 13:04:44 +0100

    Refactor RegexNameMatcherTests for better clarity
    The RegexNameMatcherTests have been refactored to be more clear and precise. The setup of unit tests and TestCases were adjusted to be more suitable for the scope of the tests. Logic in the Execute test method was modified to increase test coverage and readability of the tests.

--- SanctionsApiTest/RegexNameMatcherTests.cs
+++ SanctionsApiTest/RegexNameMatcherTests.cs
@@ -9,10 +9,6 @@ namespace SanctionsApiTest;
 [TestFixture]
 public class RegexNameMatcherTests
 {
-    private RegexNameMatcher _matcher;
-    private Mock<ILogger<SimpleNameMatcher>> _loggerMock;
-    private Mock<IWebHostEnvironment> _envMock;
-
     [SetUp]
     public void SetUp()
     {
@@ -21,32 +17,33 @@ public class RegexNameMatcherTests
         _matcher = new RegexNameMatcher(_loggerMock.Object, _envMock.Object);
     }
 
+    private RegexNameMatcher _matcher;
+    private Mock<ILogger<SimpleNameMatcher>> _loggerMock;
+    private Mock<IWebHostEnvironment> _envMock;
+
     [Test]
-    public void Execute_FindsExpectedNameInRowData_ReturnsTrue()
+    [TestCase("B Limited", "B", true)]
+    [TestCase("C Limited", "B", false)]
+    public void Execute_FindsExpectedNameInRowData_ReturnsExpectedTrue(string nameInRow, string nameToTest, bool expectedResult)
     {
         // Arrange
-        var fullNames = new List<FullName>()
-        {
-            new FullName()
-            {
-                NameParts = new List<string>{ "B", "Limited" },
-                MaxAllowedCount = 2
-            }
-        };
-        var rowData = new List<string>()
-        {
-            "ABC Limited", 
-            "A&B Limited", 
-            "A Limited", 
-            "B Limited", 
-            "ACB Limited", 
-            "B & C Limited"
-        };
-          
+        var fullNames = new List<FullName>();
+        var fullName = new FullName();
+        fullName.Names.Add(nameToTest);
+        fullNames.Add(fullName);
+
+        // "ABC Limited", 
+        // "A&B Limited", 
+        // "A Limited", 
+        // "ACB Limited", 
+        // "B & C Limited"
+
+        var rowData = new List<string> { nameInRow };
+
         // Act
         var result = _matcher.Execute(fullNames, rowData);
-        
+
         // Assert
-        Assert.True(result);
+        Assert.True(expectedResult == result);
     }
 }
